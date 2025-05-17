d70cf672d3165712ca79cf7e8116e8fec59c9b17
Timofey <Timofeygorev.steam@gmail.com>
2025-05-17 15:54:49 +0500

    change logging_promtail.py

--- AuthenticationService/src/core/logging_promtail.py
+++ AuthenticationService/src/core/logging_promtail.py
@@ -7,7 +7,6 @@ from src.core.config import settings
 logging.basicConfig(
     level=settings.log.level_value,
     format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler(sys.stdout)],
 )
 
 logger = logging.getLogger(__name__)
