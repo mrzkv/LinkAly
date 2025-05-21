818864395fd2b94df239e5bbc0907b235b56f350
Timofey <Timofeygorev.steam@gmail.com>
2025-05-21 12:35:19 +0500

    Finish base config.py

---
+++ RedirectService/src/core/config.py
@@ -0,0 +1,79 @@
+import logging
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ServerConfig(BaseSettings):
+    host: str
+    port: int
+    workers: int
+
+    model_config = SettingsConfigDict(
+        env_prefix="SERVER_",
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+class DatabaseConfig(BaseSettings):
+    user: str
+    password: str
+    db: str # Database name
+    host: str
+    port: int
+
+    pool_size: int
+    max_overflow: int
+
+    @property
+    def async_url(self) -> str:
+        return f"postgresql+asyncpg://{self.username}:{self.password}@{self.host}:{self.port}/{self.name}"
+
+    model_config = SettingsConfigDict(
+        env_prefix="POSTGRES_",
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+
+class ApiVersionConfig(BaseSettings):
+    root: str
+
+class PrefixConfig(BaseSettings):
+    v1: ApiVersionConfig
+
+class LoggingConfig(BaseSettings):
+    level: str
+
+    model_config = SettingsConfigDict(
+        env_prefix="LOGGING_",
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+    @property
+    def level_value(self) -> int:
+        return logging.getLevelNamesMapping().get(self.level.upper(), logging.DEBUG)
+
+class Settings(BaseSettings):
+    server: ServerConfig
+    db: DatabaseConfig
+    api: PrefixConfig
+    log: LoggingConfig
+
+settings = Settings(
+    server=ServerConfig(),
+    db=DatabaseConfig(),
+    api=PrefixConfig(
+        v1=ApiVersionConfig(
+            root="/v1/api",
+        ),
+    ),
+    log=LoggingConfig(),
+
+)
