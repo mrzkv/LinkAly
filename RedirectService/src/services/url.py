70d48519f08c3e2fe5183615a093e625a1bd7514
Timofey <Timofeygorev.steam@gmail.com>
2025-05-26 16:01:37 +0500

    now users can create url pair

--- RedirectService/src/services/url.py
+++ RedirectService/src/services/url.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.dao.url import UrlDAO
 
+from src.schemas.url import NewUrlPair, SuccessCreateUrlPair, SerializedUrlPair
+
 from fastapi import HTTPException
 
 class UrlService:
@@ -16,3 +18,24 @@ class UrlService:
             raise HTTPException(status_code=404)
         return f"http://{db_urls.real_url}"
 
+    async def create_new_url_pair(
+            self,
+            data: NewUrlPair,
+            creator_id: int,
+    ) -> SuccessCreateUrlPair:
+        db_urls = await self.dao.get(short_url=data.short_url)
+        if db_urls:
+            raise HTTPException(status_code=409, detail="Short url already exists")
+        url_pair = await self.dao.add(
+            SerializedUrlPair(
+                short_url=data.short_url,
+                real_url=data.short_url,
+                creator_id=creator_id,
+            )
+        )
+        return SuccessCreateUrlPair(
+            short_url=url_pair.short_url,
+            real_url=url_pair.real_url,
+            creator_id=url_pair.creator_id,
+            url_id=url_pair.id,
+        )
