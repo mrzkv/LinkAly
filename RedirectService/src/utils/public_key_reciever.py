bc3c010f5e6ba62bb96c3011190c037d4eabdd08
Timofey <Timofeygorev.steam@gmail.com>
2025-06-06 01:27:07 +0500

    fix url pairs recieve

--- RedirectService/src/utils/public_key_reciever.py
+++ RedirectService/src/utils/public_key_reciever.py
@@ -6,7 +6,7 @@ def get_public_key(url: str, default: str) -> str:
     Function to get public key from auth-service
 
     :param default: Return default public key if cant get key from auth-service
-    :param url: Url to endpoint with public key ( JWKS )
+    :param url: Url to endpoint with public key (JWKS)
     :return: public_key
     """
     try:
